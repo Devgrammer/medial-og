8c8ac688366988bcb1ea2a112bfb876ddfddcc5d
Abhinav Rastogi <rastogiabhinav@Abhinavs-MacBook-Pro.local>
2024-08-05 08:22:28 +0530

    issue fixed

--- netlify/functions/generate-og-image.js
+++ netlify/functions/generate-og-image.js
@@ -1,4 +1,5 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 exports.handler = async function (event, context) {
     if (event.httpMethod === 'OPTIONS') {
@@ -49,7 +50,11 @@ exports.handler = async function (event, context) {
             };
         }
 
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: chromium.args,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
+        });
         const page = await browser.newPage();
 
         await page.setContent(`
